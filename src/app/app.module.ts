d520cf37f4a55a0b6c4174d34576585742bc1f73
Lounes Badji <lounes.b.ext@padoa-group.com>
2022-08-29 12:19:00 +0200

    feat: angular material setup with sidenav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MenuComponent } from './menu/menu.component';
+import { MenuComponent } from './core/menu/menu.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, MenuComponent],
+  imports: [BrowserAnimationsModule, HttpClientModule, AppRoutingModule, MenuComponent],
   providers: [HttpClientModule],
   bootstrap: [AppComponent],
 })
--- src/app/core/menu/menu.component.ts
+++ src/app/core/menu/menu.component.ts
@@ -9,6 +9,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-menu',
@@ -22,6 +23,7 @@ import { RouterModule } from '@angular/router';
     MatButtonModule,
     MatIconModule,
     MatListModule,
+    LayoutModule,
     RouterModule,
   ]
 })
